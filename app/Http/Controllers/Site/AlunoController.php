fd35ab344f8df61e6a9319812c03c031aa14a306
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-04-21 07:11:34 +0100

    update painei aluno

--- app/Http/Controllers/Site/AlunoController.php
+++ app/Http/Controllers/Site/AlunoController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Formador;
 use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    public function index() 
+    public function index()
     {
-     return view('site.aluno');   
+        $professores = Formador::join("usuarios", "formadors.id_usuario", "usuarios.id_usuario")->select("formadors.*", "usuarios.*")->get();
+        return view('site.aluno',compact('professores'));
     }
 }
