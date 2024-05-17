519cbe750dab22b7611dd544635a19c0aa112ca1
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-05-17 21:03:46 +0100

    adicionar o modal ao deletar um curso

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -36,12 +36,32 @@
 
                                             <div>
                                                 </class>
-                                                <td class="cell"> <a class="btn btn-danger"  data-toggle="modal" data-target="#confirmDeleteModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                <td class="cell"> <a class="btn btn-danger" data-toggle="modal" data-target="#confirmDeleteModal{{$cursos->id_curso}}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                                             <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1
    0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0
     .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.
     5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                                                         </svg></a></td>
+                                            </div>
+
+                                            <div class="modal fade" id="confirmDeleteModal{{$cursos->id_curso}}" tabindex="-1" aria-labelledby="confirmDeleteModalLabel{{$cursos->id_curso}}" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Eliminação</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Você realmente deseja eliminar este curso?
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <a type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</a>
+                                                            <a type="button" class="btn btn-danger" href="{{ route('cursosAdmin.destroy',['id'=>$cursos->id_curso]) }}">Eliminar</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
 
 
@@ -60,23 +80,5 @@
             </div><!--//app-wrapper-->
 
 
-            <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Eliminação</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            Você realmente deseja eliminar este curso?
-                        </div>
-                        <div class="modal-footer">
-                            <a type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</a>
-                            <a type="button" class="btn btn-danger"  href="{{ route('cursosAdmin.destroy',['id'=>$cursos->id_curso]) }}" >Eliminar</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
             @endsection
